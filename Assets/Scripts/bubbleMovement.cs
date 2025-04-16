233135a82ebf7a5218806f6a67726988b3dabb53
Riwu <89302213+Riwu1219@users.noreply.github.com>
2025-04-16 21:23:10 +0800

    update bubble movement script

--- Assets/Scripts/bubbleMovement.cs
+++ Assets/Scripts/bubbleMovement.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class bubbleMovement : MonoBehaviour
 {
-    public Vector3 bubble;
+    private float x = 0, y = 0;
+    private Rigidbody rb;
+    private float magnitude;
+
+    public float minMagnitude = 0.5f;
+    public Transform bubbleTrans;
     protected Transform mouseTrans;
 
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        bubbleTrans = GetComponent<Transform>();
     }
 
     void Update()
@@ -18,8 +25,24 @@ public class bubbleMovement : MonoBehaviour
         
     }
 
+    //Used to calculate force and add force on bubble movement
     void ForceCaculation()
     {
+        bool Canceled = false;
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Calculate 
+
+            //Allow user to cancel movement if the magnitude < than configurated value
+            if (magnitude < minMagnitude)
+            {
+                Canceled = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0) && !Canceled)
+        {
+            rb.AddForce(x, y, 0);
+        }
         
     }
 }
