7b8ed6c841bb94d34d8c8e1a09e3a1160714e9c2
月が綺麗だ <tszsinglai@gmail.com>
2025-04-19 16:52:26 +0800

    AImLab_KillCountUI added

--- Assets/Scripts/DestroyOnClick.cs
+++ Assets/Scripts/DestroyOnClick.cs
@@ -1,45 +1,46 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DestroyOnClick : MonoBehaviour
 {
     public GameObject bubblePrefab;
-    public float respawnDelay = 0.3f; //RespawnDelay time 
-
-    // SpawnPosition
+    public float respawnDelay = 0.3f;
     public Vector3 minSpawnPosition;
     public Vector3 maxSpawnPosition;
+    public Text killCountText;
+
+    private int killCount = 0;
+
+    void Start()
+    {
+        UpdateKillCountUI();
+    }
 
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)); 
             RaycastHit hit;
 
-
-            if (Physics.Raycast(ray, out hit, 1000f)) //raycast detect distance
+            if (Physics.Raycast(ray, out hit, 1000f))
             {
-                Debug.Log("Hit: " + hit.collider.gameObject.name); //<<<<<<<  should will show the name of the object hit
+                Debug.Log("Hit: " + hit.collider.gameObject.name); // <<<<< should will show the name of the object hit
                 if (hit.collider != null && hit.collider.CompareTag("Bubble"))
                 {
                     Destroy(hit.collider.gameObject);
+                    killCount++;
+                    UpdateKillCountUI();
                     StartCoroutine(RespawnBubble());
                 }
-            }
-            else
-            {
-                Debug.Log("No hit detected.");
             }
         }
     }
 
-    IEnumerator RespawnBubble()
+    IEnumerator RespawnBubble() //wait for a few seconds before respawning the bubble
     {
-        // wait till the respawntime
         yield return new WaitForSeconds(respawnDelay);
-
 
         Vector3 randomPosition = new Vector3(
             Random.Range(minSpawnPosition.x, maxSpawnPosition.x),
@@ -47,7 +48,11 @@ public class DestroyOnClick : MonoBehaviour
             Random.Range(minSpawnPosition.z, maxSpawnPosition.z)
         );
 
-        // Respawn
         Instantiate(bubblePrefab, randomPosition, Quaternion.identity);
     }
-}   
+
+    void UpdateKillCountUI()
+    {
+        killCountText.text = "x" + killCount; // Update the UI text
+    }
+}
