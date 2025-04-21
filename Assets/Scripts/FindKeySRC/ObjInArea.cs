51eacbac219082071cf6c03b10e9cc5bb28e876f
skym4 <ssteveand@gmail.com>
2025-04-21 23:51:44 +0800

    Find_Key_Update

--- Assets/Scripts/FindKeySRC/ObjInArea.cs
+++ Assets/Scripts/FindKeySRC/ObjInArea.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class ObjInArea : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "Doll" && this.gameObject.name == "Box_For_Doll") 
+        if (other.gameObject.name == "Doll" && this.gameObject.name == "Box_For_Doll" && SpawnKey.instance.FinishCleanRoom == false) 
         {
                 SpawnKey.instance.DollInBox = true;
+                SpawnKey.instance.ShowKey();
+                SpawnKey.instance.DollOnTableOBJ.SetActive(true);
+                Destroy(other.gameObject);
         }
-        if (other.gameObject.name == "Garbage" && this.gameObject.name == "Bin")
+        if (other.gameObject.name == "Garbage" && this.gameObject.name == "Bin" && SpawnKey.instance.FinishCleanRoom == false)
         {
                 SpawnKey.instance.GarbageInBin = true;
+                SpawnKey.instance.ShowKey();
+                SpawnKey.instance.GarbageInBinOBJ.SetActive(true);
+                Destroy(other.gameObject);
         }
-        if (other.gameObject.name == "Key" && this.gameObject.name == "Door")
+        if (other.gameObject.name == "Key" && this.gameObject.name == "Door" && SpawnKey.instance.FinishCleanRoom == false)
         {
            Destroy(this.gameObject);
+           Destroy(this);
+           SpawnKey.instance.KeyDestrory();
 
         }
     }
--- Assets/Scripts/FindKeySRC/SpawnKey.cs
+++ Assets/Scripts/FindKeySRC/SpawnKey.cs
@@ -7,18 +7,27 @@ public class SpawnKey : MonoBehaviour
     public static SpawnKey instance ;
 
     public GameObject Key;
+    public GameObject DollOnTableOBJ;
+    public GameObject GarbageInBinOBJ;
     public bool DollInBox = false ;
     public bool GarbageInBin = false;
+    public bool FinishCleanRoom = false ;
 
     private void Start()
     {
         instance = this ;
     }
-    private void Update()
+    public void ShowKey()
     {
         if (DollInBox == true && GarbageInBin == true) 
         {
             Key.SetActive(true);
         }
+    }
+
+    public void KeyDestrory ()
+    {
+        Key.SetActive(false);
+        FinishCleanRoom = true ;
     }
 }
