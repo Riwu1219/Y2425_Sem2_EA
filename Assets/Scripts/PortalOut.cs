1e3d9f923c44ea58a6e2ad694872bdb98e39afdf
月が綺麗だ <tszsinglai@gmail.com>
2025-04-22 21:04:42 +0800

    [Platformer] Level1 + 2 updated

--- Assets/Scripts/PortalOut.cs
+++ Assets/Scripts/PortalOut.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PortalOut : MonoBehaviour
 {
-    public Transform targetPos; //The position to teleport to, set in Unity 
+    public Transform targetPos;
 
     private void OnTriggerEnter(Collider other)
     {
-        //Other "Bubble" object can transform
         if (other.CompareTag("Bubble"))
         {
+            bubblePhysics bubblePhysicsScript = other.GetComponent<bubblePhysics>();
+            if (bubblePhysicsScript != null)
+            {
+                bubblePhysicsScript.enabled = false;
 
-            other.transform.position = targetPos.position;
+                other.transform.position = targetPos.position;
+
+            }
+            else
+            {
+                  other.transform.position = targetPos.position;
+            }
         }
     }
 }
