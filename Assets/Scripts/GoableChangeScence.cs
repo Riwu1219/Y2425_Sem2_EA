fb09af7fd42f487c087be7a489c6fc4a8884411b
skym4 <ssteveand@gmail.com>
2025-04-23 18:18:35 +0800

    Change_Scence_Setting

---
+++ Assets/Scripts/GoableChangeScence.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Device;
+using UnityEngine.SceneManagement;
+public class GoableChangeScence : MonoBehaviour
+{
+    public bool ThisShooter, ThisKey, ThisPlatformer;
+    public DestroyOnClick DestroyOnClick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ThisShooter) 
+        {
+            if(DestroyOnClick.dialogueBox4.isActiveAndEnabled) 
+            {
+                StartCoroutine(DelySwitchScreen());
+            }
+        }
+    }
+
+    public void StarScenceEnd() 
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void EndScenceStart() 
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator DelySwitchScreen()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(3);
+    }
+}
