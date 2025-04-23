902d92816a07a0e667c49e4142e2117ef85065df
Riwu <89302213+Riwu1219@users.noreply.github.com>
2025-04-23 15:16:27 +0800

    update config scene

--- Assets/Scripts/ConfigSceneController.cs
+++ Assets/Scripts/ConfigSceneController.cs
@@ -8,11 +8,18 @@ public class ConfigSceneController : MonoBehaviour
 
     private Vector3 mousePosition;
 
-    [Header("Config")]
+    [Header("Component")]
     public GameObject bubblePrefab;
     public Transform spawner;
+
+    [Header("GetUI")]
     public TextMeshProUGUI volumeIndexText;
     public Animator volumeIndexAnimator;
+    public Dialogue dialogue;
+    public GameObject LoadinMenu;
+
+    [Header("Status")]
+    public bool canAdjust = false;
 
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private int volumeIndex; //Only value in 0 to 10 is allowed
@@ -24,9 +31,22 @@ public class ConfigSceneController : MonoBehaviour
         volumeIndexText.text = "" + volumeIndex;
     }
 
+    private void Start()
+    {
+        dialogue.StartFirstDialogue();
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isBubbleAlive())
+        //check if dialog end function
+        if (dialogue.triggerEvent)
+        {
+            Debug.Log("Goup");
+            LoadinMenu.GetComponent<Animator>().Play("GoUp_anim");
+            canAdjust = true;
+        }
+
+        if (Input.GetMouseButtonDown(0) && !isBubbleAlive() && canAdjust)
         {
             AdjustAudioVolume();
             PlayerPrefs.SetInt("AudioVolume", volumeIndex);
